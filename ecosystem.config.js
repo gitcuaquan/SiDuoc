191e33ab8f7bd873c827698d39e2cf5ee8e0a802
gitcuaquan <79095757+gitcuaquan@users.noreply.github.com>
2025-11-24 10:51:02 +0700

    u

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "Si Duoc V1",
+      name: "siduoc",
       port: "9999",
       exec_mode: "cluster",
       instances: "max", // Or a number of instances
